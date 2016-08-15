7e0420a8e191a41afba1e04a912b0220e8332311
Jacob Bernheim <bernheimjacob@gmail.com>
2016-08-15 19:39:39 -0400

    added scanners to make letter count interactivee

--- LetterCount.java
+++ LetterCount.java
@@ -1,14 +1,22 @@
+import java.util.Scanner;
 public class LetterCount {
 
 	public static void main (String[] args) {
-		System.out.println(count("hello", 'l'));
+		Scanner console = new Scanner(System.in);
+
+		System.out.println("Enter a phrase");
+		String text = console.nextLine();
+		System.out.println("Enter a character");
+		String c = console.nextLine();
+
+		System.out.println(count(text, c));
 	}
 
-	public static int count(String text, char c) {
+	public static int count(String text, String c) {
 		int found = 0;
 
 		for (int i = 0; i < text.length(); i++) {
-			if (text.charAt(i) == c) {
+			if (text.substring(i, i+1).equals(c)) {
 				found++;
 			}
 		}
