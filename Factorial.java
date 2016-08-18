55b19f146731da5515dc2776979800d001e266e0
Jacob Bernheim <bernheimjacob@gmail.com>
2016-08-18 13:20:05 -0400

    made factorial program

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+public class Factorial {
+	public static void main(String[] args) {
+		System.out.println(factorial(10));
+	}
+
+	// pre : n >= 0
+	// post: returns n factorial (n!)
+	public static int factorial(int n) {
+    	if (n < 0) {
+        	throw new IllegalArgumentException("negative n: " + n);
+    	}
+    	int product = 1;
+
+    	for (int i = 2; i <= n; i++) {
+    	    product *= i;
+    	}
+    	return product;
+	}
+
+}
